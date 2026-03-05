59cb846a2c0c7ed5e191419d8c84e489ffb7d0cc
Pavle <pavle1230kac@gmail.com>
2026-03-05 12:45:08 +0100

    Synced with the Brain repo on Raspberry

--- Brain/src/dashboard/frontend/src/app/webSocket/web-socket.service.ts
+++ Brain/src/dashboard/frontend/src/app/webSocket/web-socket.service.ts
@@ -70,7 +70,7 @@ export class WebSocketService {
 
   constructor() {
     this.webSocket = new Socket({
-    url: "http://192.168.88.48:5005",
+    url: "http://<RPi_IP>:5005",
     options: {},
     });
 
